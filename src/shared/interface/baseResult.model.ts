ae59205ca7a1e546dae4d826f8b207197e4ed295
sirachuch <sirachuch.kotuta@gmail.com>
2024-03-06 21:34:13 +0700

    config baseresult

--- src/shared/interface/baseResult.model.ts
+++ src/shared/interface/baseResult.model.ts
@@ -1,6 +1,13 @@
-export interface baseResult {
+export interface BaseResultObject {
     status?: boolean
     status_code?: number
     message?: string
     result?: any
+}
+
+export interface BaseResultCollection {
+    status?: boolean
+    status_code?: number
+    message?: string
+    results?: any
 }
